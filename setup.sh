4a5adc97f7392c1f5e3b188723e574ba1cbe486b
Brendan Shanny <brendan.shanny@gmail.com>
2018-11-12 18:22:14 -0800

    Add more comments. Add gruvbox

--- setup.sh
+++ setup.sh
@@ -16,13 +16,17 @@ fi
 
 if [ -d ~/.tmux/plugins ]; then
   if [ ! -d ~/.tmux/plugins/tpm ]; then
+    echo "Installing tpm"
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
   fi
   if [ ! -d ~/.tmux/plugins/tmux-pain-control ]; then
+    echo "Installing pain-control"
     git clone https://github.com/tmux-plugins/tmux-pain-control ~/.tmux/plugins/tmux-pain-control
   fi
 
 else
+  echo "Installing tpm"
+  echo "Installing pain-control"
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
   git clone https://github.com/tmux-plugins/tmux-pain-control ~/.tmux/plugins/tmux-pain-control
 fi
@@ -33,14 +37,22 @@ if [ ! -d ~/.vim/autoload ]; then
   mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 fi
 
-if [ ! "$(ls ~/.vim/bundle/)" ]; then
+if [ ! -d ~/.vim/bundle/nerdtree ]; then
+  echo "Installing nerdtree"
   git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 fi
 
+if [ ! -d ~/.vim/bundle/gruvbox ]; then
+  echo "Installing gruvbox"
+  git clone https://github.com/morhetz/gruvbox ~/.vim/bundle/gruvbox
+fi
+
 if [ ! -f ~/.vimrc ]; then
+  echo "Symlinking vimrc"
   ln -s ~/Repositories/dotfiles/.vimrc ~/.vimrc
 fi
 
 if [ ! -f ~/.tmux.conf ]; then
+  echo "Symlinking tmux.conf"
   ln -s ~/Repositories/dotfiles/.tmux.conf ~/.tmux.conf
 fi
